ca6ca178897ebf7a863a9ea38e145ce00186c5a9
Yuan <Zhi>
2014-11-18 13:19:40 -0800

    Added receipt database files

---
+++ AndroidStudioProjects/ReceiptViewer/app/src/main/java/com/bootcamp/receipt/receiptviewer/ReceiptDataBase.java
@@ -0,0 +1,19 @@
+package com.bootcamp.receipt.receiptviewer;
+
+/**
+ * Created by yuanzhi on 11/18/14.
+ */
+public class ReceiptDataBase {
+
+    /**
+     *
+     * @return Receipt ID
+     */
+    public String addDataEntry(ReceiptDAO receipt) {
+
+    }
+
+    public boolean removeDataEntry(String reciptID) {
+
+    }
+}
