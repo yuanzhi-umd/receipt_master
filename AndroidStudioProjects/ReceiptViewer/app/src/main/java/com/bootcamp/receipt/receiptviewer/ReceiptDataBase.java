009ed1662341b6dc122fd1fdba4feb8a1ead585f
Yuan <Zhi>
2014-11-18 14:04:26 -0800

    modified ReceiptDataBase.java with setter and getters, removed ReceiptDAO.java

--- AndroidStudioProjects/ReceiptViewer/app/src/main/java/com/bootcamp/receipt/receiptviewer/ReceiptDataBase.java
+++ AndroidStudioProjects/ReceiptViewer/app/src/main/java/com/bootcamp/receipt/receiptviewer/ReceiptDataBase.java
@@ -5,15 +5,59 @@ package com.bootcamp.receipt.receiptviewer;
  */
 public class ReceiptDataBase {
 
-    /**
-     *
-     * @return Receipt ID
-     */
-    public String addDataEntry(ReceiptDAO receipt) {
+    private int receiptID = 0;
+    private int timeStamp = 0;
+    private float totalAmount = 0;
+    private String description = null;
+    private String category = null;
+    private String vendor = null;
+    private String imageUrl = null;
 
+    public void setTotalAmount(float totalAmount) {
+        this.totalAmount = totalAmount;
     }
 
-    public boolean removeDataEntry(String reciptID) {
+    public float getTotalAmount() {
+        return totalAmount;
+    }
 
+    public void setTimeStamp(int timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public int getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
     }
 }
