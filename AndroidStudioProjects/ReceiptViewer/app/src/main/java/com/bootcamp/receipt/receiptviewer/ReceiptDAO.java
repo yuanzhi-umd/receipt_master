0ade6d27dec152575a135b39ad9a8312d22133fd
notyte <vkb@valeryb-macbookpro.roam.corp.google.com>
2014-11-19 11:31:43 -0800

    moving the db columns array into the db helper class

--- AndroidStudioProjects/ReceiptViewer/app/src/main/java/com/bootcamp/receipt/receiptviewer/ReceiptDAO.java
+++ AndroidStudioProjects/ReceiptViewer/app/src/main/java/com/bootcamp/receipt/receiptviewer/ReceiptDAO.java
@@ -16,15 +16,6 @@ public class ReceiptDAO {
 
     private SQLiteDatabase database;
     private ReceiptDBHelper dbHelper;
-    private String[] DATABASE_COLUMNS = {
-            "receipt_id",
-            "purchase_ts",
-            "receipt_desc",
-            "total_amount",
-            "category",
-            "vendor",
-            "image_url"
-    };
 
     public ReceiptDAO(Context context) {
         dbHelper = new ReceiptDBHelper(context);
@@ -39,8 +30,8 @@ public class ReceiptDAO {
     }
 
     public Receipt getReceipt(long receiptId) {
-        Cursor cursor = database.query(ReceiptDBHelper.TABLE_NAME,
-                DATABASE_COLUMNS, ReceiptDBHelper.RECEIPTS_TABLE_ID + " = " + receiptId, null,
+        Cursor cursor = database.query(ReceiptDBHelper.RECEIPTS_TABLE_NAME,
+                ReceiptDBHelper.DATABASE_COLUMNS, ReceiptDBHelper.RECEIPTS_TABLE_ID + " = " + receiptId, null,
                 null, null, null);
         cursor.moveToFirst();
         Receipt newReceipt = cursorToReceipt(cursor);
@@ -49,19 +40,19 @@ public class ReceiptDAO {
     }
 
     public long insertReceipt(ContentValues cv) {
-        long insertId = database.insert(ReceiptDBHelper.TABLE_NAME, null, cv);
+        long insertId = database.insert(ReceiptDBHelper.RECEIPTS_TABLE_NAME, null, cv);
         return insertId;
     }
 
-    public long insertReceipt(int purchaseTimestamp, String receiptDescription,
+    public long insertReceipt(long purchaseTimestamp, String receiptDescription,
                               int totalAmount, String category, String vendor, String imageUrl) {
         ContentValues cv = new ContentValues();
-        cv.put(DATABASE_COLUMNS[1], purchaseTimestamp);
-        cv.put(DATABASE_COLUMNS[2], receiptDescription);
-        cv.put(DATABASE_COLUMNS[3], totalAmount);
-        cv.put(DATABASE_COLUMNS[4], category);
-        cv.put(DATABASE_COLUMNS[5], vendor);
-        cv.put(DATABASE_COLUMNS[6], imageUrl);
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[1], purchaseTimestamp);
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[2], receiptDescription);
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[3], totalAmount);
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[4], category);
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[5], vendor);
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[6], imageUrl);
 
         return insertReceipt(cv);
     }
@@ -73,24 +64,24 @@ public class ReceiptDAO {
 
     public void deleteReceipt(long receiptId) {
         System.out.println("Deleting receipt with id: " + receiptId);
-        database.delete(ReceiptDBHelper.TABLE_NAME, ReceiptDBHelper.RECEIPTS_TABLE_ID
+        database.delete(ReceiptDBHelper.RECEIPTS_TABLE_NAME, ReceiptDBHelper.RECEIPTS_TABLE_ID
                 + " = " + receiptId, null);
     }
 
     public void updateReceipt(ContentValues cv) {
-        database.update(ReceiptDBHelper.TABLE_NAME, cv, ReceiptDBHelper.RECEIPTS_TABLE_ID + "=" +
+        database.update(ReceiptDBHelper.RECEIPTS_TABLE_NAME, cv, ReceiptDBHelper.RECEIPTS_TABLE_ID + "=" +
                 cv.getAsString(ReceiptDBHelper.RECEIPTS_TABLE_ID), null);
     }
 
     public void updateReceipt(Receipt receipt) {
         ContentValues cv = new ContentValues();
-        cv.put(DATABASE_COLUMNS[0], receipt.getId());
-        cv.put(DATABASE_COLUMNS[1], receipt.getTimeStamp());
-        cv.put(DATABASE_COLUMNS[2], receipt.getDescription());
-        cv.put(DATABASE_COLUMNS[3], receipt.getTotalAmount());
-        cv.put(DATABASE_COLUMNS[4], receipt.getCategory());
-        cv.put(DATABASE_COLUMNS[5], receipt.getVendor());
-        cv.put(DATABASE_COLUMNS[6], receipt.getImageUrl());
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[0], receipt.getId());
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[1], receipt.getTimeStamp());
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[2], receipt.getDescription());
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[3], receipt.getTotalAmount());
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[4], receipt.getCategory());
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[5], receipt.getVendor());
+        cv.put(ReceiptDBHelper.DATABASE_COLUMNS[6], receipt.getImageUrl());
 
         updateReceipt(cv);
     }
@@ -98,8 +89,8 @@ public class ReceiptDAO {
     public List<Receipt> getAllReceipts() {
         List<Receipt> receipts = new ArrayList<Receipt>();
 
-        Cursor cursor = database.query(ReceiptDBHelper.TABLE_NAME,
-                DATABASE_COLUMNS, null, null, null, null, null);
+        Cursor cursor = database.query(ReceiptDBHelper.RECEIPTS_TABLE_NAME,
+                ReceiptDBHelper.DATABASE_COLUMNS, null, null, null, null, null);
 
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
