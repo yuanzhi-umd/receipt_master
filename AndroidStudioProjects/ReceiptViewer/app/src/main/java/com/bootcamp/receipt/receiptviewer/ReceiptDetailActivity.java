82de99ae2edf155c5de863509f9daaea0604e710
Yuan <Zhi>
2014-11-19 17:33:48 -0800

    added camera image scaling

--- AndroidStudioProjects/ReceiptViewer/app/src/main/java/com/bootcamp/receipt/receiptviewer/ReceiptDetailActivity.java
+++ AndroidStudioProjects/ReceiptViewer/app/src/main/java/com/bootcamp/receipt/receiptviewer/ReceiptDetailActivity.java
@@ -86,9 +86,36 @@ public class ReceiptDetailActivity extends Activity {
                 Log.i(LOG_TAG, "image file exists!");
                 Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
                 ImageView myImage = (ImageView) findViewById(R.id.receiptView);
-                myImage.setImageBitmap(myBitmap);
+                // myImage.setImageBitmap(myBitmap);
+                displayReceiptScaled(myImage, photo_data_path);
             }
         }
+    }
+
+    private void displayReceiptScaled(ImageView mImageView, String mCurrentPhotoPath) {
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+        Log.i(LOG_TAG, "targetW: " + targetW);
+        Log.i(LOG_TAG, "targetH: " + targetH);
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        mImageView.setImageBitmap(bitmap);
     }
 
     @Override
