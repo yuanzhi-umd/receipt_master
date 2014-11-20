1261a9bb39c5889d903b668c2f5c788ecac626b4
Yuan <Zhi>
2014-11-19 17:12:13 -0800

    Add option to clear database

--- AndroidStudioProjects/ReceiptViewer/app/src/main/java/com/bootcamp/receipt/receiptviewer/MainActivity.java
+++ AndroidStudioProjects/ReceiptViewer/app/src/main/java/com/bootcamp/receipt/receiptviewer/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -22,6 +23,7 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -72,6 +74,18 @@ public class MainActivity extends Activity {
         if (id == R.id.action_image_capture){
             Intent intent = new Intent(this, CameraActivity.class);
             startActivityForResult(intent, 1);
+            return true;
+        }
+        if (id == R.id.clear_all_database){
+            ReceiptDAO dao = new ReceiptDAO(this);
+            dao.open();
+            List<Receipt> all_receipts = dao.getAllReceipts();
+            for (Receipt r : all_receipts) {
+                File imgFile = new File(r.getImageUrl());
+                imgFile.delete();
+                dao.deleteReceipt(r);
+            }
+            dao.close();
             return true;
         }
         return super.onOptionsItemSelected(item);
